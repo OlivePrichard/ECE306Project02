7e6261b3384b3b67f59920afc486636385218ece
Olive Prichard <70983271+ChillyCoyote273@users.noreply.github.com>
2026-02-05 15:37:50 -0500

    copied over files

---
+++ Project02/Debug/main.d
@@ -0,0 +1,48 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: /home/olive/ti/ccs1281/ccs/ccs_base/msp430/include/msp430.h
+main.obj: /home/olive/ti/ccs1281/ccs/ccs_base/msp430/include/msp430fr2355.h
+main.obj: /home/olive/ti/ccs1281/ccs/ccs_base/msp430/include/in430.h
+main.obj: /home/olive/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+main.obj: /home/olive/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+main.obj: /home/olive/ti/ccs1281/ccs/ccs_base/msp430/include/legacy.h
+main.obj: /home/olive/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/string.h
+main.obj: /home/olive/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h
+main.obj: /home/olive/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h
+main.obj: /home/olive/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h
+main.obj: /home/olive/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/xlocale/_string.h
+main.obj: ../functions.h
+main.obj: ../LCD.h
+main.obj: ../ports.h
+
+../main.c:
+
+/home/olive/ti/ccs1281/ccs/ccs_base/msp430/include/msp430.h:
+
+/home/olive/ti/ccs1281/ccs/ccs_base/msp430/include/msp430fr2355.h:
+
+/home/olive/ti/ccs1281/ccs/ccs_base/msp430/include/in430.h:
+
+/home/olive/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+/home/olive/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+/home/olive/ti/ccs1281/ccs/ccs_base/msp430/include/legacy.h:
+
+/home/olive/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/string.h:
+
+/home/olive/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h:
+
+/home/olive/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h:
+
+/home/olive/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h:
+
+/home/olive/ti/ccs1281/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/xlocale/_string.h:
+
+../functions.h:
+
+../LCD.h:
+
+../ports.h:
+
